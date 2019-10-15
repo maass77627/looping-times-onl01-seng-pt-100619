aac70c3e1844bb40c2761dee2799d01009b596c5
kelly r maass <kmaass77627@gmail.com>
2019-10-15 01:05:14 +0000

    Automatically backed up by Learn

--- times.rb
+++ times.rb
@@ -1,12 +1,12 @@
 
-
 def using_times(looping_string)
 	7.times do 
-	  puts "looping_string"
+	  puts "#{looping_string}"
+end
 end
 
 
-using_times(Wingardium Leviosa)
+using_times("Wingardium Leviosa")
 
 
 
